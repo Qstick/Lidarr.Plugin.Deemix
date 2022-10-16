838f3692e3254559176ec079a1b8afbc65bf8701
Qstick <qstick@gmail.com>
2022-10-16 09:39:19 -0500

    Update Plugin.cs

--- src/Lidarr.Plugin.Deemix/Plugin.cs
+++ src/Lidarr.Plugin.Deemix/Plugin.cs
@@ -5,7 +5,7 @@ namespace NzbDrone.Core.Plugins
     public class DeemixPlugin : Plugin
     {
         public override string Name => "Deemix";
-        public override string Owner => "ta264";
-        public override string GithubUrl => "https://github.com/ta264/Lidarr.Plugin.Deemix";
+        public override string Owner => "Q";
+        public override string GithubUrl => "https://github.com/Qstick/Lidarr.Plugin.Deemix";
     }
 }
